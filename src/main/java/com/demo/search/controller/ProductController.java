7646de181f71d73ed37ffcc02c6c2224c185b818
Isidore Dilao <iori7891@gmail.com>
2019-01-17 08:28:31 -0400

    added test, change search parameter

--- src/main/java/com/demo/search/controller/ProductController.java
+++ src/main/java/com/demo/search/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private ObjectMapper objectMapper;
     private RestTemplate restTemplate;
-    private final String bestBuyUrl = "https://api.bestbuy.com/v1/products((search=%s))?apiKey=pfe9fpy68yg28hvvma49sc89&sort=salePrice.asc&show=name,salePrice&pageSize=1&format=json";
+    private final String bestBuyUrl = "https://api.bestbuy.com/v1/products((%s))?apiKey=pfe9fpy68yg28hvvma49sc89&sort=salePrice.asc&show=name,salePrice&pageSize=1&format=json";
     private final String walmartUrl = "http://api.walmartlabs.com/v1/search?query=%s&apiKey=rm25tyum3p9jm9x9x7zxshfa&categoryId=3944&sort=price&order=asc&numItems=1";
 
     @Autowired
@@ -25,22 +31,32 @@ public class ProductController {
     }
 
     @GetMapping("/search")
-    public Product searchProduct(@RequestParam String name) {
-        Product bestBuy = getLowest("BestBuy", String.format(bestBuyUrl, name), "products");
-        Product walmart = getLowest("Walmart", String.format(walmartUrl, name), "items");
+    public ResponseEntity<String> searchProduct(@RequestParam String name) {
+        // put each word inside a 'search={query}' for better search results
+        String bestBuyQueryString = String.join("&", Arrays.stream(name.split(" ")).map(query -> "search=" + query).collect(Collectors.toList()));
+
+        // replace spaces with '+' for better search results (at least that's how it works in their developer console)
+        String walmartQueryString = name.replaceAll(" ", "+");
+
+
+        Product bestBuy = getLowest("BestBuy", String.format(bestBuyUrl, bestBuyQueryString), "products");
+        Product walmart = getLowest("Walmart", String.format(walmartUrl, walmartQueryString), "items");
+        String noResult = "No Results Found.";
 
         if (bestBuy != null && walmart != null)
-            return (bestBuy.getBestPrice() < walmart.getBestPrice()) ? bestBuy : walmart;
+            return new ResponseEntity(bestBuy.getBestPrice() < walmart.getBestPrice() ? bestBuy : walmart, HttpStatus.OK);
 
-        return bestBuy == null ? walmart : bestBuy;
+        if (bestBuy == null && walmart == null)
+            return new ResponseEntity(noResult, HttpStatus.OK);
+
+        return new ResponseEntity(bestBuy == null ? walmart : bestBuy, HttpStatus.OK);
     }
 
     private Product getLowest(String store, String url, String productsStr) {
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
-        ObjectMapper mapper = new ObjectMapper();
         Product product = null;
         try {
-            JsonNode root = mapper.readTree(responseEntity.getBody());
+            JsonNode root = objectMapper.readTree(responseEntity.getBody());
             JsonNode productsNode = root.get(productsStr);
             if (productsNode.isArray() && productsNode.size() > 0) {
                 JsonNode productNode = productsNode.get(0);
--- src/test/java/com/demo/search/controller/ProductControllerTests.java
+++ src/test/java/com/demo/search/controller/ProductControllerTests.java
@@ -3,15 +3,25 @@ package com.demo.search.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTests {
+    @Autowired
+    private MockMvc mvc;
 
     @Test
-    public void contextLoads() {
-
+    public void testSearchProduct() throws Exception {
+        mvc.perform(get("/product/search?name=NoResult")).andExpect(status().isOk())
+                .andExpect(content().string("No Results Found."));
     }
 }
